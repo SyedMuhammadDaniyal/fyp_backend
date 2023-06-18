38264e03be6a66a7c082769c1b0996189fa73b6b
Usama Ali <ussaam74@gmail.com>
2023-06-18 23:31:16 +0500

    New changes

--- sprint/models.py
+++ sprint/models.py
@@ -26,13 +26,14 @@ class Ticket(BaseModel):
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
+    github_link = models.CharField(max_length=255, null=True, blank=True)
     status = models.CharField(choices=kanban_status, max_length=30, default=TODO)
     creator = models.ForeignKey("core.User", on_delete=models.RESTRICT, related_name='creator')
     assignee = models.ForeignKey("core.User", on_delete=models.RESTRICT, related_name='assignee')
 
 class TicketLog(BaseModel):
     ticket = models.ForeignKey(Ticket, on_delete=models.RESTRICT, related_name='ticket')
-    github_link = models.CharField(max_length=255)
+    github_link = models.CharField(max_length=255, null=True, blank=True)
     to_status = models.CharField(max_length=30)
     from_status = models.CharField(max_length=30)
     mover = models.ForeignKey("core.User", on_delete=models.RESTRICT)
